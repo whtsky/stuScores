c189977b516b19dcbb28edb8e8895886fa577bd3
Margaux <154373567@qq.com>
2016-11-21 22:01:49 +0800

    exam-specified analytics

--- frontend/src/pages/Analytics.vue
+++ frontend/src/pages/Analytics.vue
@@ -8,12 +8,25 @@
   import { mapGetters } from 'vuex'
   export default {
     name: 'Classes',
+    data() {
+      return {
+        exam: 0
+      }
+    },
     computed: {
       ...mapGetters([
         'scores',
         'studentNames',
         'examNames'
       ]),
+      displayedScores() {
+        return this.scores.filter(
+          x => x.exam_id === this.exam_id
+        )
+      },
+      meanScore() {
+
+      }
     },
   }
 </script>
