6b409f2674d828618e9fbcaefd60c17a655ed1b1
Margaux <154373567@qq.com>
2016-11-21 21:55:27 +0800

    add analytics and redirect

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,13 +9,16 @@ import Classes from 'pages/Classes'
 import Exams from 'pages/Exams'
 import Subjects from 'pages/Subjects'
 import Scores from 'pages/Scores'
+import Analytics from 'pages/Analytics'
 
 const routes = [
-  { path: '/student', component: Students },
+  { path: '/student', component: Students, alias: '/' },
   { path: '/exam', component: Exams},
   { path: '/score', component: Scores},
   { path: '/class', component: Classes},
-  { path: '/subject', component: Subjects}
+  { path: '/subject', component: Subjects},
+  { path: '/analytics', component: Analytics},
+  { path: '*', redirect: '/'}
 ]
 
 const router = new VueRouter({
---
+++ frontend/src/pages/Analytics.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    I'm analytics!
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    name: 'Classes',
+    computed: {
+      ...mapGetters([
+        'scores',
+        'studentNames',
+        'examNames'
+      ]),
+    },
+  }
+</script>
