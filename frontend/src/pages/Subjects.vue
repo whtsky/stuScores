07775e5a503f44b3dbb9f22d09d01de57423fbb0
Wu Haotian <whtsky@gmail.com>
2016-12-28 11:17:06 +0800

    add/remove/change subject

--- frontend/src/pages/Subjects.vue
+++ frontend/src/pages/Subjects.vue
@@ -3,7 +3,7 @@
     <md-dialog md-open-from="#add" md-close-to="#add" ref="addDialog">
       <md-dialog-title>添加学科</md-dialog-title>
       <md-dialog-content>
-          <md-input-container>
+          <md-input-container :class="{'md-input-invalid': subjectExist}">
             <label>学科名称</label>
             <md-input required v-model="newSubject"></md-input>
             <span class="md-error">该学科已存在</span>
@@ -19,6 +19,27 @@
         </md-button>
       </md-dialog-actions>
     </md-dialog>
+
+    <md-dialog ref="changeDialog">
+      <md-dialog-title>修改学科</md-dialog-title>
+      <md-dialog-content>
+          <md-input-container :class="{'md-input-invalid': changeSubjectExist}">
+            <label>学科名称</label>
+            <md-input required v-model="currentSubjectName"></md-input>
+            <span class="md-error">该学科已存在</span>
+          </md-input-container>
+        </form>
+      </md-dialog-content>
+
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="closeChangeDialog()">取消</md-button>
+        <md-button class="md-primary" @click="changeSubject()" :disabled="!canChange">
+          <md-spinner md-indeterminate :md-size="10" v-if="changing"></md-spinner>
+          修改学科
+        </md-button>
+      </md-dialog-actions>
+    </md-dialog>
+
     <div class="main-content">
       <md-table-card>
         <md-toolbar>
@@ -29,40 +50,29 @@
         </md-toolbar>
 
         <md-table-alternate-header md-selected-label="selected">
-          <md-button class="md-icon-button">
+          <md-button class="md-icon-button" @click="removeSubjects()">
             <md-icon>delete</md-icon>
           </md-button>
         </md-table-alternate-header>
 
-        <md-table md-sort="calories">
+        <md-table @select="onSelect">
           <md-table-header>
             <md-table-row>
-              <md-table-head md-sort-by="id" md-numeric>ID</md-table-head>
-              <md-table-head md-sort-by="name">学科名称</md-table-head>
+              <md-table-head md-numeric>ID</md-table-head>
+              <md-table-head>学科名称</md-table-head>
             </md-table-row>
           </md-table-header>
 
           <md-table-body>
-            <md-table-row v-for="(row, rowIndex) in nutrition" :key="rowIndex" :md-item="row" md-selection>
-              <md-table-cell v-for="(column, columnIndex) in row" :key="columnIndex" :md-numeric="columnIndex !== 'dessert' && columnIndex !== 'comment' && columnIndex !== 'type'">
-                <span v-if="columnIndex === 'comment'">{{ column }}</span>
-
-                <md-button class="md-icon-button" v-if="columnIndex === 'comment'">
+            <md-table-row v-for="(row, rowIndex) in subjects" :key="rowIndex" :md-item="row" md-selection>
+              <md-table-cell :md-numeric="true">
+                <span>{{ row.id }}</span>
+              </md-table-cell>
+              <md-table-cell :md-numeric="false">
+                <span>{{ row.name }}</span>
+                <md-button class="md-icon-button" @click="openChangeDialog(row)">
                   <md-icon>edit</md-icon>
                 </md-button>
-
-                <md-select
-                  placeholder="Type"
-                  :name="'type' + columnIndex"
-                  :id="'type' + columnIndex"
-                  v-model="nutrition[rowIndex].type"
-                  v-if="columnIndex === 'type'">
-                  <md-option value="ice_cream">Ice Cream</md-option>
-                  <md-option value="pastry">Pastry</md-option>
-                  <md-option value="other">Other</md-option>
-                </md-select>
-
-                <span v-if="columnIndex !== 'type' && columnIndex !== 'comment'">{{ column }}</span>
               </md-table-cell>
             </md-table-row>
           </md-table-body>
@@ -74,12 +84,19 @@
 
 <script>
   import { mapGetters } from 'vuex'
+  import { API } from 'src/utils'
+  import { map } from 'lodash'
+
   export default {
     name: 'Subjects',
     data() {
       return {
         newSubject: '',
-        adding: false
+        adding: false,
+        selectedData: [],
+        currentSubjectName: "",
+        currentSubjectID: 0,
+        changing: false
       }
     },
     computed: {
@@ -93,20 +110,76 @@
         return this.subjects.some(x => x.name === this.trimedNewSubjectName)
       },
       canSubmit() {
-        return this.trimedNewSubjectName && !this.subjectExist && !this.adding
+        return this.trimedNewSubjectName && !(this.subjectExist || this.adding)
+      },
+      trimedChangeSubjectName() {
+        return this.currentSubjectName.trim()
+      },
+      changeSubjectExist() {
+        return this.subjects.some(x => x.name === this.trimedChangeSubjectName && x.id !== this.currentSubjectID)
+      },
+      canChange() {
+        return this.trimedChangeSubjectName && !(this.changeSubjectExist || this.changing)
       }
     },
     methods: {
+      onSelect(data) {
+        this.selectedData = data;
+      },
       openAddDialog() {
         this.newSubject = ''
         this.$refs['addDialog'].open()
       },
+      openChangeDialog(subject) {
+        this.currentSubjectName = subject.name
+        this.currentSubjectID = subject.id
+        this.$refs['changeDialog'].open()
+      },
       closeAddDialog() {
         this.$refs['addDialog'].close()
       },
-      addSubject() {
-        this.adding = true
+      closeChangeDialog() {
+        this.$refs['changeDialog'].close()
       },
+      addSubject() {
+        if (!this.canSubmit) {
+          return
+        }
+        this.adding = true
+        API.post('/subject', {
+          name: this.trimedNewSubjectName
+        })
+        .then((r) => {
+          console.log(r)
+          this.adding = false
+          this.closeAddDialog()
+        })
+        .catch((error) => {
+          console.error(error)
+          this.adding = false
+        });
+      },
+      changeSubject() {
+        if (!this.canChange) {
+          return
+        }
+        this.changing = true
+        API.put(`/subject/${this.currentSubjectID}`, {
+          name: this.trimedChangeSubjectName
+        })
+        .then((r) => {
+          console.log(r)
+          this.changing = false
+          this.closeChangeDialog()
+        })
+        .catch((error) => {
+          console.error(error)
+          this.changing = false
+        });
+      },
+      removeSubjects() {
+        map(this.selectedData, s => API.delete(`/subject/${s.id}`))
+      }
     }
   }
 </script>
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,5 +2,14 @@ import axios from 'axios';
 
 export const API = axios.create({
   baseURL: '/api',
-  timeout: 1000
+  timeout: 1000,
+  transformRequest(data, headers) {
+    headers['Content-Type'] = 'application/x-www-form-urlencoded'
+    const str = [];
+    for(const p in data)
+      if (data.hasOwnProperty(p) && data[p]) {
+        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(data[p]))
+      }
+    return str.join("&")
+  }
 })
